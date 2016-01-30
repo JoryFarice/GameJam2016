60e172046d2e61c03a86b9785a8dbfe2582aa2cc
Ryan Joshi <joryfarice@gmail.com>
2016-01-29 22:13:02 -0500

    Dialog Box preliminary code set up.

---
+++ PuzzlePlatformer/Assets/Scripts/DialogBoxes.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogBoxes : MonoBehaviour
+{
+	public Transform messagePrefab;
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.gameObject.tag == "Player" && (GameObject.FindGameObjectWithTag("Message")==null))
+		{
+		Instantiate(messagePrefab, new Vector2(this.transform.position.x, this.transform.position.y + 3),Quaternion.identity);
+		}
+	}
+}
+
